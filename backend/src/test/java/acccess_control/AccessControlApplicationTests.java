9ca37fccf916afc10fb0daeeba03429ea42266b7
daniel <danielrpiccoli@gmail.com>
2026-05-27 20:45:48 -0300

    setup spring boot backend project skeleton and database properties

---
+++ backend/src/test/java/acccess_control/AccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package acccess_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
