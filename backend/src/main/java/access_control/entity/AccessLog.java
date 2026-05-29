e056fd0e5299cbb9a6068639911d482aeb5877aa
daniel <danielrpiccoli@gmail.com>
2026-05-29 11:13:12 -0300

    AccessLog entity created

---
+++ backend/src/main/java/access_control/entity/AccessLog.java
@@ -0,0 +1,55 @@
+package access_control.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class AccessLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDateTime entryTimestamp;
+
+    @Column(nullable = false)
+    private boolean success;
+
+    @OneToOne
+    @JoinColumn(name = "scheduling_id", nullable = false)
+    private Scheduling scheduling;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getEntryTimestamp() {
+        return entryTimestamp;
+    }
+
+    public void setEntryTimestamp(LocalDateTime entryTimestamp) {
+        this.entryTimestamp = entryTimestamp;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public Scheduling getScheduling() {
+        return scheduling;
+    }
+
+    public void setScheduling(Scheduling scheduling) {
+        this.scheduling = scheduling;
+    }
+
+    
+}
