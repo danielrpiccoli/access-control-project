7d0a802653a1fd40d9e96e34a569c219484fe2b7
daniel <danielrpiccoli@gmail.com>
2026-05-29 11:55:28 -0300

    AppUser repository added

---
+++ backend/src/main/java/access_control/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package access_control.repository;
+
+import access_control.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    
+    AppUser findByEmail(String email);
+}
