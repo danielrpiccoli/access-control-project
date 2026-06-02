986712904bf15c78a1405b54cf37e0a52098f416
daniel <danielrpiccoli@gmail.com>
2026-06-02 00:41:18 -0300

    access_log.dart created

---
+++ frontend/lib/models/access_log.dart
@@ -0,0 +1,24 @@
+import 'package:frontend/models/scheduling.dart';
+
+class AccessLog {
+  final int id;
+  final DateTime entryTimeStamp;
+  final bool success;
+  final Scheduling scheduling;
+
+  AccessLog({
+    required this.id,
+    required this.entryTimeStamp,
+    required this.success,
+    required this.scheduling,
+  });
+
+  factory AccessLog.fromJson(Map<String, dynamic> json) {
+    return AccessLog(
+      id: json['id'],
+      entryTimeStamp: DateTime.parse(json['entryTimeStamp']),
+      success: json['success'],
+      scheduling: Scheduling.fromJson(json['scheduling']),
+      );
+  }
+}
