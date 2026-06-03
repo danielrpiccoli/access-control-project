a9dfad418543398c2581ab89634fbef41cd857c4
daniel <danielrpiccoli@gmail.com>
2026-06-03 08:33:21 -0300

    auth_services.dart created

---
+++ frontend/lib/services/auth_service.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  final String baseUrl = 'http://localhost:8080';
+
+  Future<String?> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final token = response.body;
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token);
+      return token;
+    }
+    return null;
+  }
+
+  Future<bool> register(String name, String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/auth/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+          'name': name,
+          'email': email,
+          'password': password,
+       }
+      ),
+    );
+    return response.statusCode == 201;
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
+  }
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+  }
+}
